f4fe51c0dcdaf748ac22a4014072a74a0e6df5ef
belhadj haitam <steankir.haitam@gmail.com>
2025-01-28 18:27:01 +0100

    the solution whit C++

--- Remove-duplicates-from-Sorted-Array.cpp
+++ Remove-duplicates-from-Sorted-Array.cpp
@@ -1,13 +1,13 @@
-class Solution {
+class Solution
+{
 public:
     int removeDuplicates(vector<int>& nums)
     {
-        if(nums.empty())
-            return 0;
         int i = 0;
-        int j = 1;
-        while(++j < distance(nums.begin(), nums.end()))
-            if(nums[j] != nums[i])
+        if (nums.empty())
+            return 0;
+        for (int j = 1; j < nums.size(); j++)
+            if (nums[j] != nums[i])
                 nums[++i] = nums[j];
         return i + 1;
     }
